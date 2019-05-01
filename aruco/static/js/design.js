8b201baf737755102f9a83a2343e0a9773e77b34
Igor Nurullaev <mighty.igor@yandex.ru>
2019-05-01 13:25:04 +0300

    Не помню что я сделал

--- aruco/static/js/design.js
+++ aruco/static/js/design.js
@@ -1,64 +1,64 @@
 //ПОтом убрать
 
 function expand() {
-  butt = document.getElementById("new-aruco");
-  butt.style.width = "200px";
-  butt.style.height = "235px";
-  butt.style.bottom = "125px";
-  butt.style.backgroundImage = "none";
-  butt.style.backgroundColor = "#009541";
-  butt.style.fontSize = "1.5em";
-  butt.style.cursor = "default";
-  document.getElementById("hidden").style.display = "block";
-  document.getElementById("aruco-plus").onclick = not_expand;
-  document.getElementById("aruco-plus").style.transform = "rotate(45deg)";
-  setTimeout(hide_pic, 300);
+    butt = document.getElementById("new-aruco");
+    butt.style.width = "200px";
+    butt.style.height = "235px";
+    butt.style.bottom = "125px";
+    butt.style.backgroundImage = "none";
+    butt.style.backgroundColor = "#009541";
+    butt.style.fontSize = "1.5em";
+    butt.style.cursor = "default";
+    document.getElementById("hidden").style.display = "block";
+    document.getElementById("aruco-plus").onclick = not_expand;
+    document.getElementById("aruco-plus").style.transform = "rotate(45deg)";
+    setTimeout(hide_pic, 300);
 }
 
 function hide_pic() {
-  document.getElementById("aruco-pic").style.width = "40px";
-  document.getElementById("aruco-pic").style.height = "40px";
-  document.getElementById("aruco-pic2").style.width = "40px";
-  document.getElementById("aruco-pic2").style.height = "40px";
-  document.getElementById("aruco-pic3").style.width = "40px";
-  document.getElementById("aruco-pic3").style.height = "40px";
+    document.getElementById("aruco-pic").style.width = "40px";
+    document.getElementById("aruco-pic").style.height = "40px";
+    document.getElementById("aruco-pic2").style.width = "40px";
+    document.getElementById("aruco-pic2").style.height = "40px";
+    document.getElementById("aruco-pic3").style.width = "40px";
+    document.getElementById("aruco-pic3").style.height = "40px";
 }
 
 function not_expand() {
-  butt = document.getElementById("new-aruco");
-  butt.style.cursor = "pointer";
-  butt.style.width = "60px";
-  butt.style.bottom = "50px";
-  butt.style.height = "60px";
-  butt.style.backgroundColor = "#009541";
-  butt.style.fontSize = "0";
-  document.getElementById("aruco-pic").style.width = "0px";
-  document.getElementById("aruco-pic").style.height = "0px";
-  document.getElementById("aruco-pic2").style.width = "0px";
-  document.getElementById("aruco-pic2").style.height = "0px";
-  document.getElementById("aruco-pic3").style.width = "0px";
-  document.getElementById("aruco-pic3").style.height = "0px";
-  document.getElementById("hidden").style.display = "none";
-  document.getElementById("aruco-plus").style.transform = "rotate(0deg)";
-  document.getElementById("aruco-plus").onclick = expand;
+    butt = document.getElementById("new-aruco");
+    butt.style.cursor = "pointer";
+    butt.style.width = "60px";
+    butt.style.bottom = "50px";
+    butt.style.height = "60px";
+    butt.style.backgroundColor = "#009541";
+    butt.style.fontSize = "0";
+    document.getElementById("aruco-pic").style.width = "0px";
+    document.getElementById("aruco-pic").style.height = "0px";
+    document.getElementById("aruco-pic2").style.width = "0px";
+    document.getElementById("aruco-pic2").style.height = "0px";
+    document.getElementById("aruco-pic3").style.width = "0px";
+    document.getElementById("aruco-pic3").style.height = "0px";
+    document.getElementById("hidden").style.display = "none";
+    document.getElementById("aruco-plus").style.transform = "rotate(0deg)";
+    document.getElementById("aruco-plus").onclick = expand;
 }
 
 function display_info() {
-  document.getElementById("aruco-info").style.left = "calc(100% - 520px)";
-  hide_info_group();
+    document.getElementById("aruco-info").style.left = "calc(100% - 520px)";
+    hide_info_group();
 }
 
 function hide_info() {
-  document.getElementById("aruco-info").style.left = "100%";
+    document.getElementById("aruco-info").style.left = "100%";
 }
 
 function display_info_group() {
-  document.getElementById("multi-aruco-info").style.left = "calc(100% - 520px)";
-  hide_info();
+    document.getElementById("multi-aruco-info").style.left = "calc(100% - 520px)";
+    hide_info();
 }
 
 function hide_info_group() {
-  document.getElementById("multi-aruco-info").style.left = "100%";
+    document.getElementById("multi-aruco-info").style.left = "100%";
 }
 
 function showArucoSendContent() {
@@ -66,7 +66,7 @@ function showArucoSendContent() {
     content.style.backgroundColor = "#2D6A70";
     content.style.right = "150px";
     document.getElementById("aruco-send").onclick = hideArucoSendContent;
-    setTimeout(function() {
+    setTimeout(function () {
         var content = document.getElementById("aruco-send-content");
         content.style.width = "180px";
     }, 250);
